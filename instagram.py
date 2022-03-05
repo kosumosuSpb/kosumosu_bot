99a60d73912b948a965a7fc683cebac43420dc71
kosumosu <kosumosu7air@gmail.com>
2022-03-06 01:06:34 +0300

    сделал функцию, которая определяет, какого пользователя ей передали (юзернейм или id строкой, или int, чтобы было без разницы) и возвращает user_id

--- instagram.py
+++ instagram.py
@@ -1,4 +1,4 @@
-from instagrapi import Client
+from instagrapi import Client, exceptions
 from bot_config import INST_LOGIN, INST_PASS
 from datetime import datetime
 import logging
@@ -44,11 +44,9 @@ class InstaClient(Client):
         super().__init__(**kwargs)
         self._user_kasmas = 'kasmas'  # чтобы сто раз не вводить свой ник
 
-    # user_id = inst.user_id_from_username(user)
-    # username = username_from_user_id(user_id: int) - в обратную сторону
-
     @staticmethod
     def create_and_login(login=INST_LOGIN, password=INST_PASS):
+        """Возвращает залогиненый объект класса"""
         instance = InstaClient()
         if instance.login(login, password):
             logger.info(f'Пользователь {login} авторизован успешно')
@@ -56,24 +54,50 @@ class InstaClient(Client):
         else:
             logger.info(f'Авторизовать пользователя {login} не удалось')
 
-    def save_followers(self, user: int or str):
-        """Сохранение id подписчиков в файл"""
-        if isinstance(user, str):
-            logger.info(f'Получаем id пользователя {user}...')
-            user_id = self.user_id_from_username(user)
-        else:
+    def _get_correct_user(self, user):
+        """Получает пользователя, возвращает его id"""
+        # если не указан логин, то берём логин из объекта
+        if not user:
+            # проверка авторизованности
+            if self.username:
+                user_id = self.user_id
+            # если не авторизован, то кидаем исключение
+            else:
+                logger.warning('Не указан пользователь!')
+                raise exceptions.ClientLoginRequired('Надо залогиниться!')
+
+        # если указан id
+        elif (isinstance(user, str) and user.isdigit()) or isinstance(user, int):
             user_id = user
 
+        # если указан юзернейм, надо получить id
+        else:
+            try:
+                logger.info(f'Получаем id пользователя {user}...')
+                user_id = self.user_id_from_username(user)
+            except exceptions.ClientNotFoundError:
+                logger.error(f'Пользователь не найден, возможно он не верно указан: {user}')
+                raise
+        return user_id
+
+    def save_followers(self, user=None):
+        """Сохранение id подписчиков в файл"""
+        user_id = self._get_correct_user(user)
+
         # получаем список подписчиков
+        # придёт словарь с полной инфой о подписчиках вида
+        # {'987654321': UserShort(pk='123456789', username='user_name', full_name='Имя ...'), ... }
         logger.info(f'Получаем подписчиков пользователя {user}...')
         followers = self.user_followers(user_id)
+        logger.info(f'Получено {len(followers)} подписчиков')
 
         # проверяем наличие папки под файлы, если нету - создаём
         if not os.path.isdir('inst'):
-            logger.info('Cant find inst directory, creating it...')
+            logger.info('Не нашли папку inst, создаём...')
             os.mkdir('inst')
 
         # записываем в файл
+        # сохраняем только ключи, так что у нас останутся только id пользователей
         now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         counter = 0
         with open(f'inst/{user}_followers_{now}.txt', 'w+') as f:
@@ -94,7 +118,7 @@ class InstaClient(Client):
         logger.info(f'Берём два последних файла: {f2}, {f1}')
 
         # читаем оба файла. Получим два списка вида ['49520889582\n', '51507879495\n', '50037477495\n', ... ]
-        # перевод строки потом надо не забыть убрать
+        # перевод строки потом надо не забыть убрать. Сразу не убираю, чтобы лишнюю работу не делать
         logger.info('Открываем оба файла...')
         with open(f'inst/{f2}') as file2, open(f'inst/{f1}') as file1:
             f2 = file2.readlines()
@@ -106,9 +130,10 @@ class InstaClient(Client):
 
         if not difference:
             print('Изменений в подписчиках нет')
-            return False
+            return
 
         # эти - подписались (потому что их нет в старом файле)
+        # срез с -1 потому что в конце у нас перевод строки остался
         new = [follower[:-1] for follower in difference if follower not in f2]
         new = self.get_usernames(new) if not show_id else new
 
